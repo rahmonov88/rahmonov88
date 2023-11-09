09c74565a40756e3e8c56b66fc9ea151c3bf0450
rahmo <rahmonovx88@gmail.com>
2023-11-08 22:11:06 -0500

    day27 Array

---
+++ src/day31_custom_classes/TrafficLight.java
@@ -0,0 +1,18 @@
+package day31_custom_classes;
+
+public class TrafficLight {
+
+    String light;
+
+    // We explicitly defined ANY constructor
+    // The default constructor will not be created in background
+    // Default constructor has NO PARAMETER
+//    public TrafficLight () {
+//
+//    }
+
+    public TrafficLight (String color) {
+        light = color;
+    }
+
+}
