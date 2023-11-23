e533dbd1f93f1ee80200da3b107da61ae02492ef
rahmo <rahmonovx88@gmail.com>
2023-11-22 22:16:27 -0500

    day38

---
+++ src/day38_a_abstraction_interface/create/Create.java
@@ -0,0 +1,15 @@
+package day38_a_abstraction_interface.create;
+
+public interface Create {
+
+    public abstract void read();
+    // In interface, if we have method as return type and the name
+    // those, BY DEFAULT is "public abstract" method.
+
+
+    void write();
+    // In interface, if we have method as return type and the name
+    // those, BY DEFAULT is "public abstract" method.
+
+
+}
---
+++ src/day38_a_abstraction_interface/create/Runner.java
@@ -0,0 +1,18 @@
+package day38_a_abstraction_interface.create;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        //Create obj = new Create();
+        // I cannot create the object of INTERFACES
+        // similar to abstract class
+
+        Book obj2 = new Book();
+        obj2.read();
+        obj2.write();
+
+
+
+
+    }
+}
